468277a3308faea9f18ae9fd6ac8febce673c124
i544c <leo@i544c.me>
2018-08-20 16:32:18 +0900

    Modify authUrl

--- src/config/redux-token-auth-config.js
+++ src/config/redux-token-auth-config.js
@@ -1,7 +1,7 @@
 import { generateAuthActions } from "redux-token-auth";
 
 const config = {
-  authUrl: "http://localhost:3000/auth",
+  authUrl: "https://production.shamap.ga/auth",
   userAttributes: {
     id: "id",
     name: "name",
